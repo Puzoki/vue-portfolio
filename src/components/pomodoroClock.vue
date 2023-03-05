28c7da86f962c61ce7d7178ecefeebc9e673bd22
Puzoki <kalinin.dmitriy03@gmail.com>
2023-03-05 11:11:51 +0300

    first commit

---
+++ src/components/pomodoroClock.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div class="col-3 alert alert-danger mt-3 mx-auto text-center" role="alert" id="alert">{{this.apologies}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'pomodoroClock',
+        data() {
+            return {
+                apologies: 'Извините, проект находится в процессе разработки!'
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
